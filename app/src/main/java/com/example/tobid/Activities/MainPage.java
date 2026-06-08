19ee16011d41838af7ce4722dbc8bdc8f6302f0e
Yaniv <yanivhaga2007@gmail.com>
2026-06-08 13:45:13 +0300

    Removed commenting of user creation logic and login logic

--- app/src/main/java/com/example/tobid/Activities/MainPage.java
+++ app/src/main/java/com/example/tobid/Activities/MainPage.java
@@ -2,8 +2,12 @@ package com.example.tobid.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.tobid.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +21,11 @@ public class MainPage extends AppCompatActivity implements View.OnClickListener 
     private DatabaseReference myRef;
     private FirebaseAuth mAuth;
     private FirebaseUser user;
+
+    private ImageButton ibNewBid, ibBidHistory, ibHomeButton, ibNotifications;
+    private TextView tvSearchBar;
+    private Spinner spCategory;
+
 
     // Method to reload user data when the app is resumed
     @Override
@@ -39,11 +48,34 @@ public class MainPage extends AppCompatActivity implements View.OnClickListener 
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
 
+        // Initialize buttons
+        ibNewBid = findViewById(R.id.ibNewBid);
+        ibBidHistory = findViewById(R.id.ibBidHistory);
+        ibHomeButton = findViewById(R.id.ibHomeButton);
+        ibNotifications = findViewById(R.id.ibNotifications);
+
+        spCategory = findViewById(R.id.spCategory);
+        tvSearchBar = findViewById(R.id.tvSearchBar);
     }
 
 
     @Override
     public void onClick(View v) {
-        
+        if (v == ibHomeButton) {
+            return; // Already in home
+        }
+        else if (v == ibBidHistory) {
+            Intent i = new Intent(this, SalesHistoryActivity.class);
+            startActivity(i);
+        }
+        else if (v == ibNotifications) {
+            Intent i = new Intent(this, NotificationsActivity.class);
+            startActivity(i);
+        }
+        else if (v == ibNewBid) {
+            Intent i = new Intent(this, CreateSaleActivity.class);
+            startActivity(i);
+        }
+
     }
 }
